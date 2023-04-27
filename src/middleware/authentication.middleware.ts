3584fff2207851f5930179262fe5df88d9459664
unknown <kennedyhillary6@gmail.com>
2023-04-27 04:53:28 +0100

    logger

--- src/middleware/authentication.middleware.ts
+++ src/middleware/authentication.middleware.ts
@@ -7,6 +7,7 @@ export class AuthenticationMiddleware implements NestMiddleware {
   private readonly logger = new Logger(AuthenticationMiddleware.name);
   constructor(private requestService: RequestService) {}
   use(req: Request, res: Response, next: NextFunction) {
+    this.logger.log(AuthenticationMiddleware.name);
     const userId = '123';
     this.requestService.setUserId(userId);
     next();
