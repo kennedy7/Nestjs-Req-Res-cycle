46432bcc6b7142b2c1bcdf25bb1d4c628a577809
kennedy7 <kennedyhillary6@gmail.com>
2023-05-24 19:41:54 +0100

    interceptors

--- src/interceptors/logging.interceptor.ts
+++ src/interceptors/logging.interceptor.ts
@@ -1,36 +1,46 @@
-import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from "@nestjs/common";
-import { Observable, tap } from "rxjs";
-import { AuthenticationMiddleware } from "src/middleware/authentication.middleware";
-import { RequestService } from "src/middleware/request.service";
-
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+import { AuthenticationMiddleware } from 'src/middleware/authentication.middleware';
+import { RequestService } from 'src/middleware/request.service';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-    private readonly logger = new Logger(AuthenticationMiddleware.name)
-    constructor(private  readonly requestService: RequestService){}
-    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
-        const request = context.switchToHttp().getRequest();
-        const userAgent = request.get('user-agent') || '';
-        const { ip, method, path: url } = request;
+  private readonly logger = new Logger(AuthenticationMiddleware.name);
+  constructor(private readonly requestService: RequestService) {}
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest();
+    const userAgent = request.get('user-agent') || ;
+    const { ip, method, path: url } = request;
 
-        this.logger.log(`${method} ${url} ${userAgent} ${ip}: ${context.getClass().name} 
-        ${context.getHandler().name
-        } invoked...
-        `);
-        this.logger.debug('userId', this.requestService.getUserId())
-        const now = Date.now();
-        return next.handle().pipe(tap((res)=>{
-            const response = context.switchToHttp().getResponse();
-            const {statusCode} = response
-            const contentLength = response.get('content-length')
-
-            this.logger.log(`${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip}: ${
-                Date.now() - now
-             }ms
-             `)
-        this.logger.debug('Response:', res)
-        })
-        )
-
+    this.logger.log(`${method} ${url} ${userAgent} ${ip}: ${
+      context.getClass().name
     } 
+        ${context.getHandler().name} invoked...
+        `);
+    this.logger.debug('userId', this.requestService.getUserId());
+    const now = Date.now();
+    return next.handle().pipe(
+      tap((res) => {
+        const response = context.switchToHttp().getResponse();
+        const { statusCode } = response;
+        const contentLength = response.get('content-length');
+
+        this.logger
+          .log(`${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip}: ${
+          Date.now() - now
+        }ms
+             `);
+        this.logger.debug('Response:', res);
+      }),
+    );
+  }
 }
